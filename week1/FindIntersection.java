7401585643eb48a2778c377819c2e44cfc20b6af
Anju837 <80975943+Anju837@users.noreply.github.com>
2021-03-20 21:03:32 +0530

    Add files via upload

---
+++ week1/FindIntersection.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class FindIntersection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array= {3,2,11,4,6,7};
+	int[] arr= {1,2,8,4,9,7};
+		
+	for(int i=0;i<array.length;i++)
+	{
+		for( int j=0;j<arr.length;j++ )
+		{ if(array[i]==arr[j])
+			{
+			System.out.println( array[i]+ " ");
+			
+			}
+		
+}}
+
+
+
+	}
+
+}
