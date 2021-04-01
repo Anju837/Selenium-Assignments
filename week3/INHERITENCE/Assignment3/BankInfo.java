ad878db447bf1e989cac143400403fd923c93ae1
Anju837 <80975943+Anju837@users.noreply.github.com>
2021-04-01 20:11:48 +0530

    Add files via upload

---
+++ week3/INHERITENCE/Assignment3/BankInfo.java
@@ -0,0 +1,19 @@
+package week3.day1;
+
+public class BankInfo {
+	
+	public void saving()
+	{
+		System.out.println("savings acc");
+}
+	public void fixed()
+	{
+		System.out.println("fixed deposit");
+}
+	public void deposit()
+	{
+		System.out.println("deposited");
+}
+	public static void main(String[] args) {
+	
+}}
